6f36d23abf257244513d42f30aff8bf42cbfa448
yuhaoli-95 <yuhaoli95@outlook.com>
2023-12-02 15:33:43 +0800

    [Dev] cec2013 benchmark

---
+++ python/fitness/cec2013/cec2013/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author       : Li Yuhao
+Date         : 2023-11-29 15:45:35
+LastEditTime : 2023-11-29 16:38:18
+LastEditors  : your name
+Description  : 
+FilePath     : \\WGraD\\python\\fitness\\cec2013\\cec2013\\__init__.py
+'''
+###############################################################################
+# Version: 1.1
+# Last modified on: 3 April, 2016 
+# Developers: Michael G. Epitropakis
+#      email: m_(DOT)_epitropakis_(AT)_lancaster_(DOT)_ac_(DOT)_uk 
+###############################################################################
