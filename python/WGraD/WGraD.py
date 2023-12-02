39ab3bc7f67012ef7b25f99b66c5bfe95145a230
yuhaoli-95 <yuhaoli95@outlook.com>
2023-12-02 19:58:13 +0800

    [Dev] add class ClusterResult

--- python/WGraD/WGraD.py
+++ python/WGraD/WGraD.py
@@ -3,7 +3,7 @@
 '''
 Author       : Li Yuhao
 Date         : 2023-11-29 15:43:08
-LastEditTime : 2023-11-30 15:22:14
+LastEditTime : 2023-12-02 19:50:40
 LastEditors  : your name
 Description  : 
 FilePath     : \\WGraD\\python\\WGraD\\WGraD.py
@@ -20,6 +20,38 @@ try:
     from .tools import *
 except:
     from tools import *
+
+
+class ClusterResult:
+    def __repr__(self) -> str:
+        return f"<Cluster Result> {self.num_dimension} dimension {self.num_samples} samples"
+
+    def __init__(self, X: np.ndarray, fitness: np.ndarray, num_dimension: int) -> None:
+        assert X.shape[1] == num_dimension
+        self._X = X
+        self._fitness = fitness
+
+        self.num_samples = X.shape[0]
+        self.num_dimension = num_dimension
+        return
+    
+    @property
+    def X(self) -> np.ndarray:
+        """
+        (n x d) sample matrix
+        n: the number of samples in this cluster
+        d: dimension, the number of variables
+        """
+        return self._X
+    
+    @property
+    def fitness(self) -> np.ndarray:
+        """
+        (n x 1) fitness vector
+        n: the number of samples in this cluster
+        """
+        return self._fitness
+
 
 class WGraD:
     def __init__(self, samples: np.ndarray, fitness: np.ndarray, fitness_func: Union[FunctionType, MethodType]) -> None:
@@ -64,7 +96,7 @@ class WGraD:
         
         return
     
-    def WGrad_2(self):
+    def WGrad_2(self) -> List[ClusterResult]:
         """
         
         """
@@ -94,7 +126,13 @@ class WGraD:
             dfs(i, graph, viewed, path)
             # a new cluster found
             if len(path) > 0:
-                cluster_info.append(list(path))
+                path = list(path)
+                cluster_result = ClusterResult(
+                    X = self._samples[path],
+                    fitness = self._fitness[path],
+                    num_dimension = self._num_dimension
+                )
+                cluster_info.append(cluster_result)
 
         return cluster_info
     
